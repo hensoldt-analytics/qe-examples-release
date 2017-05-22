929526cc774c963d09905eb9163b8ad3edfb741c
jerryshao <sshao@hortonworks.com>
2017-05-22 14:40:18 +0800

    Add DirectKafkaWordCount example using new Consumer API
    Change-Id: If00965ff6536b15d1c9f12cc802e62af0c7b86ce

---
+++ spark2-examples-assembly/src/main/scala/org/apache/spark/examples/streaming/NewApiDirectKafkaWordCount.scala
@@ -0,0 +1,109 @@
+package org.apache.spark.examples.streaming
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.FileInputStream
+import java.util.Properties
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+import scala.util.control.NonFatal
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Duration, StreamingContext}
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, KafkaUtils, LocationStrategies}
+
+/**
+ * How to use:
+ *
+ * spark-submit --driver-java-options="-Djava.security.auth.login.config=<path-to>/kafka_client_jaas.conf" \
+ * --master local[*] \
+ * --class org.apache.spark.examples.streaming.NewApiDirectKafkaWordCount \
+ * spark2-examples-2.1.0.jar <broker-list> <group-id> <topic-list> <additional-kafka-params>
+ *
+ * If you want to run this example under security Kafka, you should specified
+ * kafka_client_jaas.conf in both driver and executor side. Also make sure this config file is
+ * reachable in both driver and executor side.
+ *
+ * Also you should add additional consumer configurations in <additional-kafka-params> file, like:
+ *
+ * security.protocol=SASL_PLAINTEXT
+ * sasl.mechanism=GSSAPI
+ * sasl.kerberos.service.name=kafka
+ */
+object NewApiDirectKafkaWordCount {
+
+  def main(args: Array[String]): Unit = {
+    if (args.length < 3) {
+      System.err.println(
+        "NewApiDirectKafkaWordCount: bootstrap_server_list group_id topic_list [consumer.cfg]")
+      System.exit(-1)
+    }
+
+    val brokers = args(0)
+    val groupId = args(1)
+    val topics = args(2)
+
+    val kafkaParams = new mutable.HashMap[String, Object]()
+    kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+    kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+    kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+    kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer])
+
+    if (args.length > 3) {
+      val props = args(3)
+      var in: FileInputStream = null
+      try {
+        val properties = new Properties()
+        in = new FileInputStream(props)
+        properties.load(in)
+        properties.asScala.foreach { case (k, v) =>
+          if (!kafkaParams.contains(k)) {
+            kafkaParams.put(k, v)
+          }
+        }
+      } catch {
+        case NonFatal(e) => System.err.println(e)
+      } finally {
+        if (in != null) {
+          in.close()
+          in = null
+        }
+      }
+    }
+
+    val topicSet = topics.split(",").map(_.trim).filter(!_.isEmpty).toSet
+
+    val consumerStrategy = ConsumerStrategies.Subscribe[String, String](topicSet, kafkaParams)
+
+    val sparkConf = new SparkConf()
+    val ssc = new StreamingContext(sparkConf, Duration(2000L))
+
+    val stream = KafkaUtils.createDirectStream(
+      ssc, LocationStrategies.PreferBrokers, consumerStrategy)
+
+    val words = stream.flatMap { r => r.value().split(" ") }.map { r => (r, 1) }.reduceByKey(_ + _)
+
+    words.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
